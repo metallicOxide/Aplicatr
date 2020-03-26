2d96cb36099b1b1b642901a9e469c7befcd2c18c
Jerry <jerrylu1987@hotmail.com>
2020-03-27 01:56:20 +1100

    feat (Added route): Added route to get job options for frontend

--- server/routes/searchJobs.py
+++ server/routes/searchJobs.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, send_file
 from flask_restx import Resource, Api, fields
 from server.app import api, app
-from server.logic.unswScraper import extractData, login, JobTypes
+from server.logic.unswScraper import extractData, login
+from server.logic.unswJobTypes import JobTypes
+from server.logic.unswJobTypeService import getAllUNSWJobTypes
 
 search_model = api.model('Search_Model', {
   'Username': fields.String,
@@ -10,6 +12,16 @@ search_model = api.model('Search_Model', {
   'JobType': fields.String,
   'Location': fields.String,
 }, required=True)
+
+@api.route('/getJobTypes')
+class GetAllUNSWJobTypes(Resource):
+  @api.doc(description='Get the avaliable job types for UNSW.')
+  @api.response(400, 'Invalid input or error processing data encountered.')
+  @api.response(404, 'Error connecting to data source.')
+  @api.response(200, 'Data sucessfully processed and returned.')
+  def get(self):
+    jobTypeList = getAllUNSWJobTypes()
+    return {'jobTypes' : jobTypeList} , 200
 
 @api.route('/jobs')
 class SearchJobs(Resource):
