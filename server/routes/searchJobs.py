5f2f37be49cf890b3018164772d3754d92026caf
Jerry <jerrylu1987@hotmail.com>
2020-03-28 22:40:34 +1100

    feat (Added login route): added route for login

--- server/routes/searchJobs.py
+++ server/routes/searchJobs.py
@@ -12,6 +12,32 @@ search_model = api.model('Search_Model', {
   'Location': fields.String,
 }, required=True)
 
+login_model = api.model('Login_Model', {
+  'Username': fields.String,
+  'Password': fields.String,
+}, required=True)
+
+@api.route('/jobs/login')
+class UnswLogin(Resource):
+  @api.expect(login_model, validate=True)
+  @api.doc(description='Login UNSW website.')
+  @api.response(200, 'login successfully.')
+  @api.response(400, 'Invalid input or error processing data encountered.')
+  @api.response(404, 'Error connecting to data source.')
+  def post(self):
+    credentials = request.get_json()
+    username, password = credentials.values()
+    try:
+      login(username, password)
+    except ConnectionError:
+      return {'message': 'Error connecting to data source.'}, 404
+    except ValueError:
+      return {'message': 'Invalid username or password.'}, 400
+    except:
+      return {'message': 'Error logging in.'}, 400
+
+    return {'data': True}, 200
+
 @api.route('/jobs/types/unsw')
 class UnswJobTypes(Resource):
   @api.doc(description='Get the available job types for UNSW.')
@@ -19,16 +45,14 @@ class UnswJobTypes(Resource):
   def get(self):
     # TODO: Job types shouldn't be hardcoded, should fetch from website itself
     jobTypeList = getUnswJobTypes()
-    return {'jobTypes' : jobTypeList} , 200
+    return {'data' : jobTypeList} , 200
 
 @api.route('/jobs')
 class SearchJobs(Resource):
   
   @api.expect(search_model, validate=True)
   @api.doc(description='Extract job data using login credentials and search parameters.')
-  @api.response(400, 'Invalid input or error processing data encountered.')
-  @api.response(404, 'Error connecting to data source.')
-  @api.response(200, 'Data sucessfully processed and returned.')
+
   def post(self):
     search = request.get_json()
     username, password, keywords, jobtype, location = search.values()
@@ -54,4 +78,4 @@ class SearchJobs(Resource):
     
     # TODO: add database upload metadata like username, uni, etc
     # MAYBE: maybe populate db with requested jobs
-    return {'jobs': jobs}, 200
+    return {'data': jobs}, 200
