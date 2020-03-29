13a87177bea5a4237897ab02756ed4a671c366d8
jerry <jerry.lu@mendrhub.com>
2020-03-29 19:56:00 +1100

    feat (Nav): Added Nav bar

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,68 +1,77 @@
 <template>
   <div id="app">
-    <b-container> 
-      <img alt="Vue logo" src="./assets/logo.png">
+    <NavBarView/>
+    <b-container>
       <JobListView v-bind:jobList="jobList" v-bind:title="title"/>
     </b-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import JobListView from './components/JobListView.vue';
-import { JobItem } from './interfaces/bindingModels';
+  import { Component, Vue } from 'vue-property-decorator';
+  import JobListView from './components/JobListView.vue';
+  import { JobItem, Credentials } from './interfaces/bindingModels';
+  import NavBarView from './components/NavBarView.vue';
 
-@Component({
-  components: {
-    JobListView,
-  },
-})
-export default class App extends Vue {
-  jobList: Array<JobItem> = [
-    {
-      id: "1",
-      endDate: "28/03/2020",
-      jobTitle: "Full stacc dev",
-      jobCompany: "@lassian",
-      jobType: "Computer Science",
-      jobLoc: "Sydney",
-      jobDes: "Join our team of fun loving people",
-      jobUrl: "//www.google.com",
-    }, 
-    {
-      id: "2",
-      endDate: "11/04/2020",
-      jobTitle: "Python dev",
-      jobCompany: "Maccas",
-      jobType: "Computer Science",
-      jobLoc: "Canada",
-      jobDes: "We need some good ppls",
-      jobUrl: "//www.google.com",
+  @Component({
+    components: {
+      JobListView,
+      NavBarView
     },
-    {
-      id: "3",
-      endDate: "05/03/2020",
-      jobTitle: "Frontend dev",
-      jobCompany: "NASA",
-      jobType: "Computer Science",
-      jobLoc: "Sydney",
-      jobDes: "Angular experience preferred",
-      jobUrl: "//www.google.com",
-    }, 
-    {
-      id: "4",
-      endDate: "16/12/2020",
-      jobTitle: "Backend dev",
-      jobCompany: "Canva",
-      jobType: "Computer Science",
-      jobLoc: "Sydney",
-      jobDes: "C# Asp.Net Dev",
-      jobUrl: "//www.google.com",
-    }
-  ];
+  })
+  export default class App extends Vue {
+    jobList: Array<JobItem> = [
+      {
+        id: "1",
+        endDate: "28/03/2020",
+        jobTitle: "Full stacc dev",
+        jobCompany: "@lassian",
+        jobType: "Computer Science",
+        jobLoc: "Sydney",
+        jobDes: "Join our team of fun loving people",
+        jobUrl: "//www.google.com",
+      }, 
+      {
+        id: "2",
+        endDate: "11/04/2020",
+        jobTitle: "Python dev",
+        jobCompany: "Maccas",
+        jobType: "Computer Science",
+        jobLoc: "Canada",
+        jobDes: "We need some good ppls",
+        jobUrl: "//www.google.com",
+      },
+      {
+        id: "3",
+        endDate: "05/03/2020",
+        jobTitle: "Frontend dev",
+        jobCompany: "NASA",
+        jobType: "Computer Science",
+        jobLoc: "Sydney",
+        jobDes: "Angular experience preferred",
+        jobUrl: "//www.google.com",
+      }, 
+      {
+        id: "4",
+        endDate: "16/12/2020",
+        jobTitle: "Backend dev",
+        jobCompany: "Canva",
+        jobType: "Computer Science",
+        jobLoc: "Sydney",
+        jobDes: "C# Asp.Net Dev",
+        jobUrl: "//www.google.com",
+      }
+    ];
 
-  title = 'Job List';
-}
+    title = 'Job List';
+
+    credentials: Credentials = {
+      Username:"",
+      Password:"",
+      Uni:""
+    };
+    
+  }
 </script>
 
 <style>
@@ -73,5 +82,9 @@ export default class App extends Vue {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#app {
+  margin-top: 0px;
 }
 </style>
---
+++ website/src/components/NavBarView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="NavBar">
+
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-brand href="#">Job Calendar Gen</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>Hi, User</em>
+            </template>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue } from 'vue-property-decorator';
+
+  export default class NavBar extends Vue {
+
+  }
+</script>
+
+
+<style scoped>
+
+</style>
