3f1c482738d74b7c233c8dfc472b56111953fc25
jerry <jerry.lu@mendrhub.com>
2020-03-29 16:39:29 +1100

    refactor (Job Item): Formatting fixes

--- website/src/components/JobItemView.vue
+++ website/src/components/JobItemView.vue
@@ -14,53 +14,53 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { JobItem } from '../interfaces/bindingModels';
-import moment from 'moment';
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import { JobItem } from '../interfaces/bindingModels';
+  import moment from 'moment';
 
-@Component
-export default class JobItemView extends Vue {
-  @Prop() private jobItem!: JobItem;
-  // styling array to decide what style the cards should be 
-  stylingArr: string[] = ["low-pior", "med-pior", "high-pior", "expired-pior"];
+  @Component
+  export default class JobItemView extends Vue {
+    @Prop() private jobItem!: JobItem;
+    // styling array to decide what style the cards should be 
+    stylingArr: string[] = ["low-pior", "med-pior", "high-pior", "expired-pior"];
 
-  get formattedJobTitle (): string {
-    const job: JobItem = this.jobItem;
-    return `${job.jobTitle} - ${job.jobCompany}`;
-  }
-
-  get daysTillExpiry(): number {
-    const now = moment();
-    const endDate = moment(this.jobItem.endDate, "DD/MM/YYYY");
-    return endDate.diff(now, 'days');
-  }
-
-  get getPiorityStyling(): string {
-    const days = this.daysTillExpiry;
-    // if the floor is >= 3 then return 2 (low pior)
-    // floor is <= 0 then return 0 (high pior)
-    if (days >=30) {
-      return this.stylingArr[0];
-    } else if (days >= 20) {
-      return this.stylingArr[1];
-    } else if (days >= 10) {
-      return this.stylingArr[2];
-    } else {
-      return this.stylingArr[3];
+    get formattedJobTitle (): string {
+      const job: JobItem = this.jobItem;
+      return `${job.jobTitle} - ${job.jobCompany}`;
     }
-  }
 
-  get dayLeftMsg(): string {
-    const days = this.daysTillExpiry;
-
-    if (days >= 0) {
-      return `${this.jobItem.endDate} - ${this.daysTillExpiry} days left!`;
-    } else {
-      return `${this.jobItem.endDate} - Expired :'(`;
+    get daysTillExpiry(): number {
+      const now = moment();
+      const endDate = moment(this.jobItem.endDate, "DD/MM/YYYY");
+      return endDate.diff(now, 'days');
     }
-  }
 
-}
+    get getPiorityStyling(): string {
+      const days = this.daysTillExpiry;
+      // if the floor is >= 3 then return 2 (low pior)
+      // floor is <= 0 then return 0 (high pior)
+      if (days >=30) {
+        return this.stylingArr[0];
+      } else if (days >= 20) {
+        return this.stylingArr[1];
+      } else if (days >= 10) {
+        return this.stylingArr[2];
+      } else {
+        return this.stylingArr[3];
+      }
+    }
+
+    get dayLeftMsg(): string {
+      const days = this.daysTillExpiry;
+
+      if (days >= 0) {
+        return `${this.jobItem.endDate} - ${this.daysTillExpiry} days left!`;
+      } else {
+        return `${this.jobItem.endDate} - Expired :'(`;
+      }
+    }
+
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
