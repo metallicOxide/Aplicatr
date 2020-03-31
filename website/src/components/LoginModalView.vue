6068028c23a1084b50898ddab59388c1b29883d9
jerry <jerry.lu@mendrhub.com>
2020-03-31 14:59:49 +1100

    feat (Added Modal UI): Modal UI

--- website/src/components/LoginModalView.vue
+++ website/src/components/LoginModalView.vue
@@ -1,8 +1,47 @@
 <template>
   <div class="LoginModal">
-    <b-container> 
-      <JobListView v-bind:jobList="jobList" v-bind:title="title"/>
-    </b-container>
+    <b-button v-b-modal.modal-login>Open Modal</b-button>
+    <b-modal
+      id="modal-login"
+      ref="modal"
+      size="lg"
+      title="Login"
+      header-bg-variant="primary"
+      header-text-variant="light"
+      centered = "true"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          :state="nameState"
+          label="Login Name"
+          label-for="id-input"
+          invalid-feedback="Login Name is required"
+        >
+          <b-form-input
+            id="id-input"
+            v-model="name"
+            :state="nameState"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+          :state="nameState"
+          label="Login Name"
+          label-for="id-input"
+          invalid-feedback="Login Name is required"
+        >
+          <b-form-input
+            id="id-input"
+            v-model="name"
+            :state="nameState"
+            required
+          ></b-form-input>
+        </b-form-group>
+      </form>
+    </b-modal>
   </div>
 </template>
 
@@ -11,7 +50,11 @@
 
   @Component
   export default class LoginModal extends Vue {
-    @Prop() private title!: string;
+    @Prop() private username!: string;
+    @Prop() private password!: string;
 
+    resetModal() {
+      console.log("OPen modal");
+    }
   }
 </script>
