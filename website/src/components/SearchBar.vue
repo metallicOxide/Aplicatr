28aae68d8848a691c1f73dad442c5723a2c78c64
jerry <jerry.lu@mendrhub.com>
2020-04-03 23:01:14 +1100

    feat (Search Bar): Made stub for search bar component

---
+++ website/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="SearchBar">
+      Search term is {{searchTerm}}
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class SearchBar extends Vue {
+    @Prop() private searchTerm!: string;
+    
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
