10a04bbf393d75cd624b8eb37831642192f1ec2c
jerry <jerry.lu@mendrhub.com>
2020-04-05 00:21:45 +1100

    fix (NavBar): Removed unnecessary elements

--- website/src/components/NavBarView.vue
+++ website/src/components/NavBarView.vue
@@ -4,22 +4,6 @@
     <b-navbar toggleable="lg" type="dark" variant="primary">
       <b-navbar-brand href="#"><b>Job Calendar Gen</b></b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em class="user"><b>Hi, User</b></em>
-            </template>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
     </b-navbar>
   </div>
 </template>
