f323feefdd5c7a8f7ddae19e72238d76ebf749b1
jerry <jerry.lu@mendrhub.com>
2020-04-05 02:50:03 +1100

    fix (Eslint): Remove Enforced Camel Case

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "camelcase": "off",
+    "@typescript-eslint/camelcase": ["error", { "properties": "never" }]
   }
 }
