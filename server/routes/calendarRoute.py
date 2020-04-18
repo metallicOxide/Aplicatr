f95ef60111a29e0404e59e188fcfd2e146955f48
Justin <justinnhan@hotmail.com>
2020-04-18 20:14:10 +1000

    Changed calendar response from string to binary form

--- server/routes/calendarRoute.py
+++ server/routes/calendarRoute.py
@@ -52,11 +52,7 @@ class CalendarRoute(Resource):
     except:
       return {'message': 'Error generating calendar from jobs.'}, 400
 
-    # response = make_response(pickle.dumps(str(calendar)))
-    # response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
+    response = make_response(pickle.dumps(str(calendar)))
+    response.headers["Content-Disposition"] = "attachment; filename=calendar.ics"
         
-    # return response, 200
-    
-    # TODO: Change the backend to serve the calendar file instead of the calendar string 
-    calendarObj = {'calendar': str(calendar)}
-    return calendarObj, 200
+    return response, 200
